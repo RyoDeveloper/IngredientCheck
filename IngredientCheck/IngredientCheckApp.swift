23fe98e8e24dc890ade904169cd095ad497b1709
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2023-03-20 12:00:14 +0900

    Initial Commit

---
+++ IngredientCheck/IngredientCheckApp.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientCheckApp.swift
+//  IngredientCheck
+//
+//  https://github.com/RyoDeveloper/IngredientCheck
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct IngredientCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
